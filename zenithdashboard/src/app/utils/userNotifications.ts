fb0ec8835be13465dd6d2ce5e9fc45ce30159635
Mutesi-alin <alinemutesiii@gmail.com>
2024-09-26 19:34:24 +0300

    add files

--- zenithdashboard/src/app/utils/userNotifications.ts
+++ zenithdashboard/src/app/utils/userNotifications.ts
@@ -1,35 +1,4 @@
 
-// const url= '/api/notifications/'
-
-// export const fetchNotifications = async () => {
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     return await response.json();
-//   } catch (error) {
-//     return error;
-//   }
-// };
-// export const createNotification = async (message: string) => {
-//   try {
-//     const response = await fetch(url, {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({ message }),
-//     });
-//     if (!response.ok) {
-//       throw new Error(`HTTP error! status: ${response.status}`);
-//     }
-//     return await response.json();
-//   } catch (error) {
-//     return null;
-//   }
-// };
-
 
 
 import { useState, useEffect } from 'react';
@@ -49,7 +18,7 @@ export const useNotifications = () => {
         const data = await response.json();
         setNotifications(data);
       } catch {
-        // Error handled silently
+        
       }
     };
 
